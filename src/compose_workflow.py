b6d2d23ec79dd34f9da55160f2b724e323c52a2d
vedantthapa <thapavedant@gmail.com>
2023-01-13 16:46:08 -0400

    feat(src): add process-git-artifacts TemplateRef to compose_workflow.py

--- src/compose_workflow.py
+++ src/compose_workflow.py
@@ -1,17 +1,24 @@
-from hera import Task, Workflow, WorkflowService
+from hera import Task, Workflow, WorkflowService, TemplateRef, Parameter
 
 from config import NAMESPACE, TOKEN
-from tasks import fail_random, say
+from tasks import say
 
 ws = WorkflowService(
     host="https://localhost:2746", verify_ssl=False, token=TOKEN, namespace=NAMESPACE
 )
 
 
-with Workflow("diamond-workflow-", service=ws, generate_name=True) as w:
+with Workflow("hera-demo-", service=ws, generate_name=True) as w:
     a = Task("a", say, ["This is task A!"])
     b = Task("b", say, ["This is task B!"])
-    c = Task("c", fail_random)
+    c = Task(
+        "c",
+        template_ref=TemplateRef(
+            name="execute-script-template",
+            template="process-git-artifacts",
+        ),
+        inputs=[Parameter(name="revision", value="main")],
+    )
     d = Task("d", say, ["This is task D!"])
 
     a >> [b, c] >> d
